99f1c64a5179aed4100b6ced0b672bf49b395ce1
guipleite <guipleite@gmail.com>
2019-03-25 17:17:12 -0300

    Dois botoes funcionando

--- Comunicacao/HC05-Controle-Exemplo/PC_Python/youtube_controller.py
+++ Comunicacao/HC05-Controle-Exemplo/PC_Python/youtube_controller.py
@@ -5,77 +5,77 @@ import time
 import logging
 
 class MyControllerMap:
-    def __init__(self):
-        self.button = {'A': 'L'} # Fast forward (10 seg) pro Youtube
-        self.button = {'B': 'M'} # Fast forward (10 seg) pro Youtube
+	def __init__(self):
+		self.button = {'A':'Ctrl','B': 'M'} # Fast forward (10 seg) pro Youtube
+	  # Fast forward (10 seg) pro Youtube
 class SerialControllerInterface:
-    # Protocolo
-    # byte 1 -> Botão 1 (estado - Apertado 1 ou não 0)
-    # byte 2 -> EOP - End of Packet -> valor reservado 'X'
+	# Protocolo   sssddsMMMMMssMM
+	# byte 1 -> Botão 1 (estado - Apertado 1 ou não 0)
+	# byte 2 -> EOP - End of Packet -> valor reservado 'X'
 
-    def __init__(self, port, baudrate):
-        self.ser = serial.Serial(port, baudrate=baudrate)
-        self.mapping = MyControllerMap()
-        self.incoming = '0'
-        pyautogui.PAUSE = 0  ## remove delay
-    
-    def update(self):
-        ## Sync protocol
-        while self.incoming != b'X':
-            self.incoming = self.ser.read()
-            logging.debug("Received INCOMING: {}".format(self.incoming))
+	def __init__(self, port, baudrate):
+		self.ser = serial.Serial(port, baudrate=baudrate)
+		self.mapping = MyControllerMap()
+		self.incoming = '0'
+		pyautogui.PAUSE = 0  ## remove delay
+	
+	def update(self):
+		## Sync protocolMMMMMMMMMMMMMMMMMMMMMMMMMMM
+		while self.incoming != b'X':
+			self.incoming = self.ser.read()
+			logging.debug("Received INCOMING: {}".format(self.incoming))
 
-        data = self.ser.read()
-        logging.debug("Received DATA: {}".format(data))
+		data = self.ser.read()
+		logging.debug("Received DATA: {}".format(data))
 
-        if data == b'1':
-            data = self.ser.read()
-            if data == b'L':
-                logging.info("KEYDOWN A")
-                pyautogui.keyDown(self.mapping.button['A'])
-            if data == b'M':
-                logging.info("KEYDOWN B")
-                pyautogui.keyDown(self.mapping.button['B'])
+		if data == b'1':
+			data = self.ser.read()
+			if data == b'L':
+				logging.info("KEYDOWN A")
+				pyautogui.hotkey(self.mapping.button['A'])
+			if data == b'M':
+				logging.info("KEYDOWN B")
+				pyautogui.keyDown(self.mapping.button['B'])
 
-        elif data == b'0':
-            logging.info("KEYUP A")
-            pyautogui.keyUp(self.mapping.button['A'])
-            logging.info("KEYDOWN B")
-            pyautogui.keyUp(self.mapping.button['B'])
+		elif data == b'0':
+			logging.info("KEYUP A")
+			pyautogui.keyUp(self.mapping.button['A'])
+			logging.info("KEYDOWN B")
+			pyautogui.keyUp(self.mapping.button['B'])
 
 
 
-        self.incoming = self.ser.read()
+		self.incoming = self.ser.read()
 
 
 class DummyControllerInterface:
-    def __init__(self):
-        self.mapping = MyControllerMap()
+	def __init__(self):
+		self.mapping = MyControllerMap()
 
-    def update(self):
-        pyautogui.keyDown(self.mapping.button['A'])
-        time.sleep(0.1)
-        pyautogui.keyUp(self.mapping.button['A'])
-        logging.info("[Dummy] Pressed A button")
-        time.sleep(1)
+	def update(self):
+		pyautogui.keyDown(self.mapping.button['A'])
+		time.sleep(0.1)
+		pyautogui.keyUp(self.mapping.button['A'])
+		logging.info("[Dummy] Pressed A button")
+		time.sleep(1)
 
 
 if __name__ == '__main__':
-    interfaces = ['dummy', 'serial']
-    argparse = argparse.ArgumentParser()
-    argparse.add_argument('serial_port', type=str)
-    argparse.add_argument('-b', '--baudrate', type=int, default=9600)
-    argparse.add_argument('-c', '--controller_interface', type=str, default='serial', choices=interfaces)
-    argparse.add_argument('-d', '--debug', default=False, action='store_true')
-    args = argparse.parse_args()
-    if args.debug:
-        logging.basicConfig(level=logging.DEBUG)
+	interfaces = ['dummy', 'serial']
+	argparse = argparse.ArgumentParser()
+	argparse.add_argument('serial_port', type=str)
+	argparse.add_argument('-b', '--baudrate', type=int, default=9600)
+	argparse.add_argument('-c', '--controller_interface', type=str, default='serial', choices=interfaces)
+	argparse.add_argument('-d', '--debug', default=False, action='store_true')
+	args = argparse.parse_args()
+	if args.debug:
+		logging.basicConfig(level=logging.DEBUG)
 
-    print("Connection to {} using {} interface ({})".format(args.serial_port, args.controller_interface, args.baudrate))
-    if args.controller_interface == 'dummy':
-        controller = DummyControllerInterface()
-    else:
-        controller = SerialControllerInterface(port=args.serial_port, baudrate=args.baudrate)
+	print("Connection to {} using {} interface ({})".format(args.serial_port, args.controller_interface, args.baudrate))
+	if args.controller_interface == 'dummy':
+		controller = DummyControllerInterface()
+	else:
+		controller = SerialControllerInterface(port=args.serial_port, baudrate=args.baudrate)
 
-    while True:
-        controller.update()
+	while True:
+		controller.update()
